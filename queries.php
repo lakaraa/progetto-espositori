5d148185d58d0d5cde40c0640fbd3b9375211ded
ManarMashaal <manar.mashaal.studenti@isii.it>
2025-03-21 19:39:46 +0100

    aggiunta query getUserByEmail

--- queries.php
+++ queries.php
@@ -39,3 +39,11 @@ function getContributi($pdo) {
     $stmt->execute();
     return $stmt->fetchAll(PDO::FETCH_ASSOC); 
 }
+
+function getUserByEmail($pdo, $email) {
+    $sql = "SELECT * FROM utente WHERE Email = :email";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
