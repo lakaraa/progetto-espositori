a5cff5dbb800e2575e67f6a19886dbfa7e81a4b9
ManarMashaal <manar.mashaal.studenti@isii.it>
2025-04-02 23:34:30 +0200

    Aggiunta pagina di registrazione per espositori con gestione upload CV

--- pages/registrazione_espositore.php
+++ pages/registrazione_espositore.php
@@ -5,7 +5,7 @@ include_once '../template_header.php';
 <!-- Breadcrumbs-->
 <section class="breadcrumbs-custom bg-image context-dark" style="background-image: url(images/bg-breadcrumbs-03-1920x480.jpg);">
     <div class="container">
-        <h2 class="breadcrumbs-custom-title">Registration</h2>
+        <h2 class="breadcrumbs-custom-title">Registrazione</h2>
     </div>
         <ul class="breadcrumbs-custom-path">
             <li><a href="index.php">Home</a></li>
@@ -18,11 +18,11 @@ include_once '../template_header.php';
     <div class="container">
         <div class="row row-50 justify-content-center">
             <div class="col-md-10 col-lg-8">
-                <h3>Register as an Exhibitor</h3>
+                <h3>Registrati come Espositore</h3>
                 <?php if (!empty($message)): ?>
                     <p style="color: red;"><?php echo $message; ?></p>
                 <?php endif; ?>
-                <form method="post" enctype="multipart/form-data" action="../auth/register_handler.php">
+                <form method="post" enctype="multipart/form-data" action="../auth/registrazione_espositore_handler.php">
                     <div class="row row-30">
                         <div class="col-md-6">
                             <div class="form-wrap">
@@ -38,13 +38,13 @@ include_once '../template_header.php';
                         </div>
                         <div class="col-md-6">
                             <div class="form-wrap">
-                                <label class="form-label" for="registration-first-name">First Name</label>
+                                <label class="form-label" for="registration-first-name">Nome</label>
                                 <input class="form-input" id="registration-first-name" type="text" name="first_name" required>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-wrap">
-                                <label class="form-label" for="registration-last-name">Last Name</label>
+                                <label class="form-label" for="registration-last-name">Cognome</label>
                                 <input class="form-input" id="registration-last-name" type="text" name="last_name" required>
                             </div>
                         </div>
@@ -56,17 +56,17 @@ include_once '../template_header.php';
                         </div>
                         <div class="col-md-6">
                             <div class="form-wrap">
-                                <label class="form-label" for="registration-phone">Phone</label>
+                                <label class="form-label" for="registration-phone">Telefono</label>
                                 <input class="form-input" id="registration-phone" type="text" name="phone" required>
                             </div>
                         </div>
                         <div class="col-md-12">
                             <div class="form-wrap">
-                                <label class="form-label" for="registration-qualification">Qualification</label>
+                                <label class="form-label" for="registration-qualification">Qualifica</label>
                                 <select class="form-input" id="registration-qualification" name="qualification" required >
-                                    <option value="professional" style="color: black; background-color: white;">Professional</option>
-                                    <option value="amateur" style="color: black; background-color: white;">Amateur</option>
-                                    <option value="expert" style="color: black; background-color: white;">Expert Non-Professional</option>
+                                    <option value="professional" style="color: black; background-color: white;">Professionista</option>
+                                    <option value="amateur" style="color: black; background-color: white;">Amatore</option>
+                                    <option value="expert" style="color: black; background-color: white;">Esperto Non-Professionista</option>
                                 </select>
                             </div>
                         </div>
@@ -77,7 +77,7 @@ include_once '../template_header.php';
                             </div>
                         </div>
                         <div class="col-md-12">
-                            <button class="button button-primary" type="submit">Register</button>
+                            <button class="button button-primary" type="submit">Registrati</button>
                         </div>
                     </div>
                 </form>
