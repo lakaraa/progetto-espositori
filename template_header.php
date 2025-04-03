ed5994c7f43fad5ab9a0f9645cf284bc3e2da343
Sara Petrovska <sara.petrovska.studenti@isii.it>
2025-04-03 10:24:14 +0200

    .

--- template_header.php
+++ template_header.php
@@ -45,7 +45,7 @@
                             <div class="rd-navbar-main-element">
                                 <div class="rd-navbar-nav-wrap">
                                     <ul class="rd-navbar-nav">
-                                        <li class="rd-nav-item active"><a class="rd-nav-link" href="index.php">Home</a></li>
+                                        <li class="rd-nav-item"><a class="rd-nav-link" href="index.php">Home</a></li>
                                         <li class="rd-nav-item"><a class="rd-nav-link" href="manifestazione.php">Manifestazioni</a></li>
                                         <li class="rd-nav-item"><a class="rd-nav-link" href="/progetto-espositori/pages/about.php">About</a></li>
                                         <li class="rd-nav-item"><a class="rd-nav-link" href="/progetto-espositori/pages/contacts.php">Contacts</a></li>
