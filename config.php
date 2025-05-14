1423d25c87d5acae08c0959537402a7b57d30d85
Sara Petrovska <sara.petrovska.studenti@isii.it>
2025-05-14 11:53:38 +0200

    modifica config

--- config.php
+++ config.php
@@ -7,5 +7,5 @@ $db_password = 'espositori.123';
 $pdo = new PDO(dsn: "mysql:host=$_db_host;dbname=$db_dbnam", username: $db_username, password: $db_password);
 $pdo->setAttribute(attribute: PDO::ATTR_ERRMODE, value: PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(attribute: PDO::ATTR_DEFAULT_FETCH_MODE, value: PDO::FETCH_ASSOC);
-include_once('index.php');
+
 ?>
