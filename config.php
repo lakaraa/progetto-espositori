0b4d96d8e9c5305d9e4b7c5baf55eef4012dacb0
ManarMashaal <manar.mashaal.studenti@isii.it>
2025-05-17 02:22:34 +0200

    include header e footer template

--- config.php
+++ config.php
@@ -8,5 +8,7 @@ $pdo = new PDO("mysql:host=$_db_host;dbname=$db_dbname", $db_username, $db_passw
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-echo "Connessione riuscita!";
+#echo "Connessione riuscita!";
+include 'template_header.php';
+include 'template_footer.php';
 ?>
