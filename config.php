50e4a2366c0135989e40d261852150275ad1489c
ManarMashaal <manar.mashaal.studenti@isii.it>
2025-05-17 02:31:58 +0200

    tolto template

--- config.php
+++ config.php
@@ -9,6 +9,5 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
 #echo "Connessione riuscita!";
-include 'template_header.php';
-include 'template_footer.php';
+
 ?>
