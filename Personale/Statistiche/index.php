832adb4921f56b74954bdfde5078ee99a4a2fcc1
ManarMashaal <manar.mashaal.studenti@isii.it>
2025-05-22 11:56:22 +0200

    z

--- Personale/Statistiche/index.php
+++ Personale/Statistiche/index.php
@@ -4,19 +4,4 @@ include_once '../../config.php';
 include_once '../../queries.php';
 include_once '../../session.php';
 
-// Query partecipanti per mese
-$sqlPartecipanti = getQueryPartecipantiPerMese();
-
-// Query contributi per manifestazione
-$sqlContributi = getQueryContributiPerManifestazione();
-
-// Query espositori per manifestazione
-$sqlEspositori = getQueryEspositoriPerManifestazione();
-
-// Query prenotazioni per data
-$sqlPrenotazioni = getQueryPrenotazioniPerData();
-
-print_r($sqlPartecipanti);
-print_r($sqlContributi);
-print_r($sqlEspositori);
-print_r($sqlPrenotazioni);
+print_r($_SESSION);
